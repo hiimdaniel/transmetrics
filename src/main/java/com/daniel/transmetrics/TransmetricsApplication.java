99bc5e3b9130a9add42d7dcb6c48efb796561511
daniel-kollanyi <daniel.kollanyi@falcon.io>
2021-05-01 15:28:54 +0200

    Initial commit

---
+++ src/main/java/com/daniel/transmetrics/TransmetricsApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.transmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransmetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransmetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/transmetrics/TransmetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.transmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransmetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
