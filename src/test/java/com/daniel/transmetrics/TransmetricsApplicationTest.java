b802a41b2d28a0ed24a01901172d1a5da6944374
daniel-kollanyi <daniel.kollanyi@falcon.io>
2021-05-01 15:49:03 +0200

    Set up docker for local running and flyway

---
+++ src/test/java/com/daniel/transmetrics/TransmetricsApplicationTest.java
@@ -0,0 +1,15 @@
+package com.daniel.transmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TransmetricsApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
